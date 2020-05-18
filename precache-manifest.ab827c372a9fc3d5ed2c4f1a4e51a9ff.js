3f11bed1751f772d287d0ea50c87de9e5e7fcba6
SS <amoli1411vora@gmail.com>
2020-05-19 00:41:33 +0530

    Updates

---
+++ precache-manifest.ab827c372a9fc3d5ed2c4f1a4e51a9ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ddba9eb789266f6cb6f288b4cc1ca3",
+    "url": "/admindashboard/index.html"
+  },
+  {
+    "revision": "36fbeb346273b5fe97bf",
+    "url": "/admindashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e8d6e9c01d99b47f254",
+    "url": "/admindashboard/static/js/2.54f12137.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/admindashboard/static/js/2.54f12137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fbeb346273b5fe97bf",
+    "url": "/admindashboard/static/js/main.7462b08f.chunk.js"
+  },
+  {
+    "revision": "3a34726de08061ac2c7a",
+    "url": "/admindashboard/static/js/runtime-main.51792912.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/admindashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
